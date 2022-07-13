5d228dee78797a047b3a2ea081a9a4f5c7e361c2
chaker-sidhom <chaker.sidhom@vinted.com>
2022-07-13 13:08:32 +0200

    feat: add maven build and execution steps

---
+++ README.md
@@ -0,0 +1,17 @@
+# Template Generation Using `docx4j`
+
+## Requirements
+
+- Java 11.x
+- Maven
+
+## Clean Build
+```
+mvn clean
+mvn install
+```
+
+## Execution Steps
+```
+java -jar target/docx4j-example-1.0-jar-with-dependencies.jar
+```
--- src/main/java/org/docx4j/example/ConvertExample.java
+++ src/main/java/org/docx4j/example/ConvertExample.java
@@ -1,21 +1,8 @@
 package org.docx4j.example;
 import java.io.File;
-import java.io.OutputStream;
-import java.net.URI;
 
-
-import org.docx4j.Docx4J;
-import org.docx4j.XmlUtils;
-import org.docx4j.convert.out.FOSettings;
-import org.docx4j.fonts.BestMatchingMapper;
-import org.docx4j.fonts.IdentityPlusMapper;
-import org.docx4j.fonts.Mapper;
-import org.docx4j.fonts.PhysicalFont;
-import org.docx4j.fonts.PhysicalFonts;
-import org.docx4j.jaxb.Context;
-import org.docx4j.model.fields.FieldUpdater;
-import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 
 public class ConvertExample {
     public static void main(String[] args) throws Exception {
